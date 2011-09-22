baaccb84370275b4f0a5f41de941e0786121cb44
Ed Manlove <devPyPlTw@verizon.net>
2011-09-22 15:26:11 +0000

    Refactored cut/copy/paste selenium test to use page objects.
    svn path=/Products.CMFPlone/branches/selenium-integration/; revision=52284

--- Products/CMFPlone/tests/selenium/testCutCopyPaste.py
+++ Products/CMFPlone/tests/selenium/testCutCopyPaste.py
@@ -34,3 +34,58 @@ class TestCutPaste(unittest.TestCase):
         click(self.driver, "link=Folder 2")
         click(self.driver, "link=Contents")
         click(self.driver, "folder_paste:method")
+
+
+import time
+from plone.seleniumtesting.homepage import HomePage
+from plone.seleniumtesting.folderpageobject import FolderPageObject
+
+class TestCutPasteUsingPageObjects(unittest.TestCase):
+    layer = SELENIUM_PLONE_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.driver = self.layer['selenium']
+        self.portal = self.layer['portal']
+        self.driver.implicitly_wait(5)
+
+        self.hp = HomePage(self.driver, self.portal)
+
+        self.portal.acl_users._doAddUser('member1', 'secret',
+                                         ['Member'], [])
+        applyProfile(self.layer['portal'], 'Products.CMFPlone:plone-selenium')
+
+    def test_cut_and_paste_using_pageobjects(self):
+        login(self.driver,self.portal,'member1','secret')
+        self.hp.open_default_url()
+        self.driver.find_element_by_partial_link_text('Folder 1').click()
+
+        fpo = FolderPageObject(self.driver)
+        folder_contents = fpo.goto_contents_tab()
+        folder_contents.selectFolderItemByPostition(0)
+        folder_contents.cut()
+
+        #ToDo verify cut
+        
+        self.driver.find_element_by_partial_link_text('Folder 2').click()
+        folder_contents = fpo.goto_contents_tab()
+        folder_contents.paste()
+
+        #ToDo verify pasted
+        
+    def test_copy_and_paste_using_pageobjects(self):
+        login(self.driver,self.portal,'member1','secret')
+        self.hp.open_default_url()
+        self.driver.find_element_by_partial_link_text('Folder 1').click()
+
+        fpo = FolderPageObject(self.driver)
+        folder_contents = fpo.goto_contents_tab()
+        folder_contents.selectFolderItemByPostition(0)
+        folder_contents.copy()
+
+        #ToDo verify copy
+        
+        self.driver.find_element_by_partial_link_text('Folder 2').click()
+        folder_contents = fpo.goto_contents_tab()
+        folder_contents.paste()
+
+        #ToDo verify pasted
