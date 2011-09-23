9a3a723fdd36b42aff247ab2cd7af26a3905ea17
Ed Manlove <devPyPlTw@verizon.net>
2011-09-23 14:12:12 +0000

    Added edit content test using page objects.
    svn path=/Products.CMFPlone/branches/selenium-integration/; revision=52320

--- Products/CMFPlone/tests/selenium/testEditContent.py
+++ Products/CMFPlone/tests/selenium/testEditContent.py
@@ -22,3 +22,35 @@ class TestEditContent(unittest.TestCase):
         clear(self.driver, "title")
         type(self.driver,  "title", "Some say potato")
         click(self.driver, "form.button.save")
+
+
+from plone.seleniumtesting.homepage import HomePage
+from plone.seleniumtesting.folderpageobject import FolderPageObject
+
+class TestEditContentUsingPageObjects(unittest.TestCase):
+    layer = SELENIUM_PLONE_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.driver = self.layer['selenium']
+        self.portal = self.layer['portal']
+        self.driver.implicitly_wait(5)
+
+        self.hp = HomePage(self.driver, self.portal)
+
+        self.portal.acl_users._doAddUser('member1', 'secret',
+                                         ['Member'], [])
+        applyProfile(self.layer['portal'], 'Products.CMFPlone:plone-selenium')
+
+    def test_edit_content_using_pageobjects(self):
+        login(self.driver, self.portal, 'member1','secret')
+        self.hp.open_default_url()
+        self.driver.find_element_by_partial_link_text('Folder 1').click()
+
+        fpo = FolderPageObject(self.driver)
+        fpo.goto_edit_tab()
+
+        fpo.edit.title.clear()
+        fpo.edit.title.set("Some say potato")
+        fpo.edit.save()
+
+        #ToDo verify title change
